722957b089f6a10f478c8b5966f65dee53cb217f
LuisCarlos <carlosrm0994@gmail.com>
2023-10-10 17:04:54 -0500

    Se modifico el html para creación de un nuevo elemento con sus respectivos estilos en css, en JS se avanzo en la construccion de la funcion que va a crear en el body del html el archivo nuevo a crear

--- js/interfaz.js
+++ js/interfaz.js
@@ -1,19 +1,52 @@
 const optionsDetails = document.querySelectorAll(".dots-details");
 const details = document.querySelector(".main-details");
 
+const buttonNewElement = document.querySelector("#new-element");
+const menuNewElement = document.querySelector(".menu-new-elements");
+const closeMenuNewElement = document.querySelector(".close-menu-new-element");
+const createNewElement = document.querySelector("#create-new-element");
+
+const tableBody = document.querySelector(".docs-body");
+
 optionsDetails.forEach((element) =>
   element.addEventListener("click", function saludar() {
     console.log("hola");
   })
 );
 
+buttonNewElement.addEventListener("click", openOptionsNewDocs);
+function openOptionsNewDocs() {
+  menuNewElement.classList.remove("inactive");
+}
 
+closeMenuNewElement.addEventListener("click", closeMenuCreateNewElement);
+function closeMenuCreateNewElement() {
+  menuNewElement.classList.add("inactive");
+}
 
+createNewElement.addEventListener("submit", addNewElement);
+function addNewElement (event){
+  event.preventDefault();
+  
+  const nameNewElement = document.querySelector("#name-new-element").value;
+  const optionsTypeElement = document.getElementsByName("options-elements");
+  
+  let optionSelect = undefined;
+  for(let i = 0; i < optionsTypeElement.length; i++){
+    if (optionsTypeElement[i].checked){
+      optionSelect = optionsTypeElement[i].value;
+      break;
+    }
+  }
 
-
-
-
-
+  let elementToCreate = {
+    name: nameNewElement,
+    type: optionSelect,
+    date: new Date(),
+  }
+  
+  console.log("Creando nuevo elemento. Nombre:", nameNewElement, "Opción seleccionada:", optionSelect, "Objeto creado:", elementToCreate);
+}
 
 // <tr>
 //   <td><input type="checkbox"></td>
